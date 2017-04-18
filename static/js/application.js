07689ea1a24e27e31bf1a4981eddc2665536b74f
Ben <ben@notionparallax.co.uk>
2017-04-19 03:59:26 +1000

    Fix client name

--- static/js/application.js
+++ static/js/application.js
@@ -38,7 +38,7 @@ outbox.onclose = function(){
 
 $("#input-form").on("submit", function(event) {
   event.preventDefault();
-  var handle = $("#input-handle")[0].value;
+  var handle = "turkBrain" //$("#input-handle")[0].value;
   var text   = $("#input-text")[0].value;
   outbox.send(JSON.stringify({ handle: handle, text: text }));
   $("#input-text")[0].value = "";
